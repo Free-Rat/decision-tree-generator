adf38e709a31d4849982c9728bb60d69176a7277
Free-Rat <lawicki02@gmail.com>
2024-10-15 21:06:38 +0200

    basic data file parsing

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("Welcome to Decision Tree Generatro")
+
+def get_data_from_file(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        print(content)
+        rows = content.split("\n")
+        print(rows)
+        data = []
+        for idx in range(len(rows)):
+            row = rows[idx].split(",")
+            data.append(row)
+        return data
+
+data = get_data_from_file('data.csv')
+for row in data:
+    print("")
+    for val in row:
+        print(val, end="\t")
+
+    
+
