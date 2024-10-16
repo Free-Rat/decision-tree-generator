e4582bbe4406b93ddba84eb45facf92388590ad9
Free-Rat <lawicki02@gmail.com>
2024-10-16 21:57:01 +0200

    print tree

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ print("Welcome to Decision Tree Generatro")
 
 # zwraca słownik ktory ma strukture 
 # {
+#     ...
 #     <nazwa_wartości_atrybutu>: [ 
 #         <ilość_decyzji_yes>,
 #         <lista_indeksów_z_tym_parameterem>
 #     ]
+#     ...
 # }
 def count_atribure_values(atribute, decision):
     atribute_dir = {}
@@ -26,9 +28,12 @@ def intrinsic_info(atribute_dir):
     values = list(atribute_dir.values())
     res = 0
     all_vals_count = sum(len(sublist[1]) for sublist in values)
+    # print(all_vals_count)
     for val in values:
         a = len(val[1])/all_vals_count
+        # print(a)
         res -= a * log2(a)
+        # print(a * log2(a))
     return res
 
 
@@ -81,29 +86,85 @@ def print_matrix(matrix):
         print("")
     print("\n")
 
+def choose_atribute(data):
+    best_atribute = (None, None, None)
+    bast_gain_ratio = 0
+    for idx in range(0, len(data)-1):
+        # print(data[idx][0])
+        atribute_dir = count_atribure_values(data[idx], data[-1])
+        gain = 1 - entropy_for_atribute(atribute_dir)
+        # print(f"Gain for {data[idx][0]} {gain}")
+        info = intrinsic_info(atribute_dir)
+        # print(f"Intrinsic_info for {data[idx][0]} {info}")
+        # print(atribute_dir)
+        gain_ratio = gain / info
+        # print(f"gain ratio {gain_ratio}")
+        # print()
+        if gain_ratio > bast_gain_ratio:
+            bast_gain_ratio = gain_ratio
+            best_atribute = (data[idx][0], atribute_dir, gain_ratio)
+    # print(f"best atriburte {best_atribute[0]}\n{best_atribute[1]}")
+    return best_atribute
+
+def filrt_data_for_atribute(data, atribute):
+    data =  [row for row in data if row[0] != atribute[0] ]
+    data = transpose_matrix(data)
+    # print_matrix(data)
+    data_set = []
+    for i in range(0, len(atribute[1])):
+        new_data = [data[i] for i in list(atribute[1].values())[i][1]]
+        new_data.insert(0, data[0])
+        # print_matrix(new_data)
+        new_data = transpose_matrix(new_data)
+        data_set.append(new_data)
+    return data_set
+
+def get_dominant_decision(dictionary):
+    values = [v[0] for v in dictionary.values()]
+    mean = sum(values) / len(values)
+    # print(values)
+    # print(dictionary)
+    return round(mean)
+
+def is_only_one_left(dictionary):
+    return len(dictionary.values()) == 1
+
+
+def subtree(data):
+    # print("\nsubtree ")
+    # print_matrix(transpose_matrix(data))
+    # print(data)
+    if len(data[-1]) == 2:
+        return decision_to_int(data[-1][-1])
+    choosen = choose_atribute(data)
+    if choosen[2] >= 1.0:
+        return get_dominant_decision(choosen[1]) 
+        
+    branches = {}
+    for n_data, name in zip(filrt_data_for_atribute(data, choosen),
+                            choosen[1].keys()):
+        # print("name of branch",name)
+        # print(n_data)
+        branches[name] = subtree(n_data)
+        # print("branches", branches)
+        # print()
+    return [choosen[0], branches]
+
 def main():
     data = get_data_from_file('data.csv')
     print_matrix(data)
     data = transpose_matrix(data)
-    # print_matrix(data)
-
-    best_atribute = (None, None)
-    bast_gain_ratio = 0
-    for idx in range(0, len(data)-1):
-        print(data[idx][0])
-        atribute_dir = count_atribure_values(data[idx], data[-1])
-        gain = 1 - entropy_for_atribute(atribute_dir)
-        print(f"Gain for {data[idx][0]} {gain}")
-        info = intrinsic_info(atribute_dir)
-        print(f"Intrinsic_info for {data[idx][0]} {info}")
-        gain_ratio = gain / info
-        print(f"gain ratio {gain_ratio}")
-        print()
-        if gain_ratio > bast_gain_ratio:
-            bast_gain_ratio = gain_ratio
-            best_atribute = (data[idx][0], atribute_dir)
-    print(f"best atriburte {best_atribute[0]}")
-
+    tree = subtree(data)
+    print("\n\nTREE:")
+    print(tree)
+    
+    
+    exit()
+    best_a = choose_atribute(data)
+    new_data = filrt_data_for_atribute(data, best_a)
+    sec_best = choose_atribute(new_data)
+    new_data = filrt_data_for_atribute(new_data, sec_best)
+    third_best = choose_atribute(new_data)
 
 
 if __name__ == "__main__":
