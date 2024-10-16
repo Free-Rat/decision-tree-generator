3155f4e09443e952eafea44a08ff87d75c4def19
Free-Rat <lawicki02@gmail.com>
2024-10-16 16:51:02 +0200

    firs iteration of best attribute

--- main.py
+++ main.py
@@ -22,22 +22,19 @@ def count_atribure_values(atribute, decision):
     # print(atribute_dir)
     return atribute_dir
 
-def intrinsic_info(values):
-    print(values[0])
+def intrinsic_info(atribute_dir):
+    values = list(atribute_dir.values())
     res = 0
     all_vals_count = sum(len(sublist[1]) for sublist in values)
-    print(all_vals_count)
-    for i in values[1]:
-        res += i/all_vals_count
-    print(res)
-
-    return 
-    # for val in values:
-    #     up = 
+    for val in values:
+        a = len(val[1])/all_vals_count
+        res -= a * log2(a)
+    return res
 
 
-def entropy_for_atribute(values):
-    print(values)
+def entropy_for_atribute(atribute_dir):
+    values = atribute_dir.values()
+    # print(values)
     all_vals_count = sum(len(sublist[1]) for sublist in values)
     
     entropy_for_atribute = 0
@@ -52,12 +49,13 @@ def entropy_for_atribute(values):
 # y to jest ilość yes'ow 
 # a to ilość wszystkich pojawień się tej wartości 
 def entropy(y, a):
-    # print(y, a)
-    if y == 0: return 0
+    # print(y, a, (a-y))
+    if y == 0 or y == a: return 0
     return -(y/a * log2(y/a)) -((a-y)/a * log2((a-y)/a))
 
 def decision_to_int(decision):
     return int(decision.lower() == "yes")
+    # return int(decision.lower() == "1")
 
 
 def transpose_matrix(matrix):
@@ -89,13 +87,24 @@ def main():
     data = transpose_matrix(data)
     # print_matrix(data)
 
+    best_atribute = (None, None)
+    bast_gain_ratio = 0
     for idx in range(0, len(data)-1):
         print(data[idx][0])
         atribute_dir = count_atribure_values(data[idx], data[-1])
-        # print("entropia " ,entropy_for_atribute(atribute_dir))
-        gain = 1 - entropy_for_atribute(atribute_dir.values())
+        gain = 1 - entropy_for_atribute(atribute_dir)
         print(f"Gain for {data[idx][0]} {gain}")
-        # print(intrinsic_info(atribute_dir.values()))
+        info = intrinsic_info(atribute_dir)
+        print(f"Intrinsic_info for {data[idx][0]} {info}")
+        gain_ratio = gain / info
+        print(f"gain ratio {gain_ratio}")
+        print()
+        if gain_ratio > bast_gain_ratio:
+            bast_gain_ratio = gain_ratio
+            best_atribute = (data[idx][0], atribute_dir)
+    print(f"best atriburte {best_atribute[0]}")
+
+
 
 if __name__ == "__main__":
     main()
